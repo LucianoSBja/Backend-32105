7266bca21c0e2d737315a1b3524eae3d70b5ee5d
Luciano Ezequiel Soto Bonja <lucianosotobonja95@gmail.com>
2022-10-25 18:31:01 -0300

    Desafio DB

---
+++ Desafio 8/dbConnection/db.js
@@ -0,0 +1,13 @@
+const connection = {
+    client: 'mysql',
+    connection:
+    {
+        host: "localhost",
+        user: "luciano",
+        password: "_pLhyp/@9HkqJBnU",
+        database: "ecommerce",
+        port: 3306
+    }
+};
+
+export default connection;
